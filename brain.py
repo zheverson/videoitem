94c9fc1fda6420f71a41230ae4508887d3aa8087
zheverson <172329313@qq.com>
2016-01-10 21:06:28 +0800

    fix brain

--- brain.py
+++ brain.py
@@ -30,30 +30,4 @@ categories = {
 
 brandslower = {''.join(i.split(' ')).lower() for i in brands}
 
-data = []
 
-desc = re.finditer(r'(.+?)\n{2,}', c, re.S)
-for i in desc:
-    p = re.finditer(r'(.+?)\n', i.group(1), re.S)
-    try:
-        wordsrow = ''.join(re.split('[\W]', next(p).group(1).lower()))
-        if not any((word in wordsrow for word in {'items', 'products'})):
-            continue
-        else:
-            for j in p:
-                row = ''.join(re.split('[\W]', j.group(1).lower()))
-                try:
-                    brand = next((word for word in brandslower if word in row))
-                    try:
-                        category = next((cat for cat in categories if cat in row))
-                        data.append({'brand': brand, 'category': category, 'name': j.group(1)})
-                    except StopIteration:
-                        data.append({'brand': brand, 'name': j.group(1)})
-                except StopIteration:
-                    try:
-                        category = next((cat for cat in categories if cat in row))
-                        data.append({'category': category, 'name': j.group(1)})
-                    except StopIteration:
-                        data.append({'name': j.group(1)})
-    except StopIteration:
-        pass
